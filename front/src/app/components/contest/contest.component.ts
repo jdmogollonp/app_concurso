12e8dbbbb28fdd2f83de9cd362013103366cc4f6
mateo <mateo.sanabria.a@gmail.com>
2020-02-12 13:18:42 -0500

    front: services contest

--- front/src/app/components/contest/contest.component.ts
+++ front/src/app/components/contest/contest.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { ContestService } from 'src/app/services/contest/contest.service';
 @Component({
   selector: 'app-contest',
   templateUrl: './contest.component.html',
@@ -9,12 +9,26 @@ import { ActivatedRoute } from '@angular/router';
 export class ContestComponent implements OnInit {
 
   public url = '';
+  videos: any = [];
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute, private contestService: ContestService) {
     this.url = this.activatedRoute.snapshot.params.url;
   }
 
+
   ngOnInit() {
+    this.contestService.getVideos(this.url)
+      .subscribe(
+        res => {
+          this.videos = res;
+        },
+        err => console.error(err)
+      );
+      console.log(this.videos)
   }
+
+
+
+
 
 }
