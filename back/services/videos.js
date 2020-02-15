078e8f04ea1a2a9f4b5dfc3f87db55be1436fb19
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2020-02-15 16:11:53 -0500

    Major modifications for cron better performance

--- back/services/videos.js
+++ back/services/videos.js
@@ -7,25 +7,25 @@ class VideosService {
     }
 
     // Gets the video information
-    getVideoInfo( videoId, idcontest) {
-       return new Promise(async (resolve, reject) => {
-           try {
-               const connection = await mysql.connect();
-               const query = `SELECT status, creation_date, message, original_video, converted_video, contestant_id FROM ${this.table} WHERE id = ? AND contest_id = ?`;
-               connection.query(query, [videoId, idcontest], (err, results, fields) => {
-                   if (err) {
-                       console.log(err);
-                       return reject(err);
-                   } else {
-                       resolve(results[0]);
-                   }
-               });
-               connection.release();
-           } catch (error) {
-               console.log(error);
-               reject(error);
-           }
-       });
+    getVideoInfo(videoId, idcontest) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const connection = await mysql.connect();
+                const query = `SELECT status, creation_date, message, original_video, converted_video, contestant_id FROM ${this.table} WHERE id = ? AND contest_id = ?`;
+                connection.query(query, [videoId, idcontest], (err, results, fields) => {
+                    if (err) {
+                        console.log(err);
+                        return reject(err);
+                    } else {
+                        resolve(results[0]);
+                    }
+                });
+                connection.release();
+            } catch (error) {
+                console.log(error);
+                reject(error);
+            }
+        });
     }
     // Gets all the videos from an contest
     getVideosContestant(idcontest) {
@@ -52,12 +52,12 @@ class VideosService {
 
 
     // Creates a new video
-    createVideo(contestId, contestantId, originalVideo, message) {
+    createVideo(contestId, originalVideo, message, email, name, lastName) {
         return new Promise(async (resolve, reject) => {
             try {
                 const connection = await mysql.connect();
-                let query = `INSERT INTO ${this.table} (contest_id, contestant_id, original_video, message) VALUES (?, ?, ?, ?);`;
-                connection.query(query, [contestId, contestantId, originalVideo, message], (err, results, fields) => {
+                let query = `INSERT INTO ${this.table} (contest_id, original_video, message, email, name, last_name) VALUES (?, ?, ?, ?, ?, ?);`;
+                connection.query(query, [contestId, originalVideo, message, email, name, lastName], (err, results, fields) => {
                     if (err) {
                         console.log(err);
                         return reject(err);
