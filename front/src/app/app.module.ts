2677d6ffd9da931730e910d1b6f05418750d3f8a
mateo <mateo.sanabria.a@gmail.com>
2020-02-18 22:04:25 -0500

    Create contest

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { ContestService } from './services/contest/contest.service'
+import { ContestService } from './services/contest/contest.service';
+import { ConstestAdministrationService }  from './services/contest-administration/constest-administration.service'
 // import { ShowVideosComponent } from './components/contest/components/contest/show-videos/show-videos.component';
 // Pagination Module
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +22,8 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     NgbModule
   ],
   providers: [
-    ContestService
+    ContestService,
+    ConstestAdministrationService
   ],
   bootstrap: [AppComponent]
 })
---
+++ front/src/app/components/contests-administration/create-contest/create-contest.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ConstestAdministrationService } from '../../../services/contest-administration/constest-administration.service'
+
+@Component({
+  selector: 'app-create-contest',
+  templateUrl: './create-contest.component.html',
+  styleUrls: ['./create-contest.component.scss']
+})
+export class CreateContestComponent implements OnInit {
+
+    public loading = false;
+
+    public contestUpload = {
+      url: '',
+      start_date: '',
+      end_date: '',
+      description: ''
+    };
+
+    public uploadedImage: File;
+
+
+  constructor(private constestAdministrationService: ConstestAdministrationService) { }
+
+  ngOnInit() {
+  }
+
+  getFileDetails(event: any) {
+    this.uploadedImage = event.target.files[0];
+  }
+
+  submitForm(event) {
+    event.preventDefault();
+    this.loading = true;
+    this.constestAdministrationService.uploadContest(this.uploadedImage, this.contestUpload).then((data: string) => {
+      window.scrollTo(0, 0);
+      // window.location.reload();
+      this.loading = false;
+      window.alert(data);
+    }).catch(err => {
+      this.loading = false;
+      window.alert(err);
+    });
+  }
+
+}
